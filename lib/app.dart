9b6df823dc67aa7ebc322f2c80a40855822352e2
Maiara Chagas <chagass.maiara@gmail.com>
2024-03-05 22:25:10 -0300

    feat: Configurando web

--- lib/app.dart
+++ lib/app.dart
@@ -64,7 +64,7 @@ class MyApp extends StatelessWidget {
         ),
       ],
       child: MaterialApp(
-        title: 'Flutter Demo',
+        title: 'Project StarWars',
         themeMode: ThemeMode.dark,
         darkTheme: customDarkTheme(),
         theme: customLightTheme(),
