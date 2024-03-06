d22b27f6bbf68238f8c132d81bfe6e8334417753
Maiara Chagas <chagass.maiara@gmail.com>
2024-03-05 21:58:42 -0300

    feat: Removendo o banner debug

--- lib/app.dart
+++ lib/app.dart
@@ -64,6 +64,7 @@ class MyApp extends StatelessWidget {
         ),
       ],
       child: MaterialApp(
+        debugShowCheckedModeBanner: false,
         title: 'Flutter Demo',
         themeMode: ThemeMode.dark,
         darkTheme: customDarkTheme(),
